18ed76776915d2586128caf95d2285075a313849
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-06-03 11:29:42 -0500

    Updated MongoDB settings with TLS options

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -82,11 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'recipeApp',
-        'ENFORCE_SCHEMA': False,
         'CLIENT': {
             'host': config('DATABASE_URL'),
-            'tls': True,  # Enable TLS
-            'tlsAllowInvalidCertificates': True  # Allow invalid certificates
+            'tls': True,
+            'tlsAllowInvalidCertificates': True
         }
     }
 }
