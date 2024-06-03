8f95e7b00dc0f395c03b7e3dec2c95d6bea9fac7
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-06-03 09:57:10 -0500

    chore: Update Django settings for Heroku deployment

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hwcvzj&1nejqo5(=$qugcm041+2(c*c2=q$2a&5cp!c+vb=j9-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-hwcvzj&1nejqo5(=$qugcm041+2(c*c2=q$2a&5cp!c+vb=j9-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,3 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #AUTH
 LOGIN_URL='/login/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
